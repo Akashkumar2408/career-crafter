b2976a84ee1d8d6e132eb29ed192655d143fca7c
akash <ap9097284@gmail.com>
2024-01-21 15:46:13 +0000

    login done

--- app/src/main/java/com/example/careercraftier/signupPage.java
+++ app/src/main/java/com/example/careercraftier/signupPage.java
@@ -2,7 +2,9 @@ package com.example.careercraftier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class signupPage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class signupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+    }
+    public void clickstudent(View view)
+    {
+        Intent intent=new Intent(signupPage.this,studentsignuppage.class);
+        startActivity(intent);
+    }
+    public  void clickadministration(View view)
+    {
+        Intent intent=new Intent(signupPage.this,administrationsignuppage.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/careercraftier/studentsignuppage.java
@@ -0,0 +1,14 @@
+package com.example.careercraftier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class studentsignuppage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentsignuppage);
+    }
+}
